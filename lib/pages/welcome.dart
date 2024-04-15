5c5fd73852caf6fce753c7ef90af21b6a6968329
Akanksha <email@example.comKrishiUrja3320@gmail.com>
2024-04-16 01:01:16 +0530

    added about us page code

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 
 import 'screen.dart';
@@ -74,7 +72,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           padding: const EdgeInsets.all(50),
           child: ElevatedButton(
             onPressed: () {
-              // Navigate to AuthPage
+              // Navigate to Screen page
               Navigator.of(context).push(
                 MaterialPageRoute(builder: (context) => const Screen()),
               );
@@ -87,12 +85,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               minimumSize: const Size(300, 20),
               padding: const EdgeInsets.all(22),
             ),
-            child: Text(
-              'Get Started',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
+            child: const Text('Get Started',
+              style: TextStyle(color: Colors.black),
             ),
           ),
         ),
